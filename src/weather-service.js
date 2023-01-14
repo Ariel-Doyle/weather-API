d31d0476e62e1db57610d755f296db9265a0a578
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-13 22:25:23 -0800

    update .js files and add giphy file to practice chaining promises per the lesson.

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,18 +1,35 @@
 export default class WeatherService {  
-  static async getWeather(city) {
-    try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
-      const jsonifiedResponse = await response.json();
-      if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
-        throw new Error(errorMessage);
-      }
-      return jsonifiedResponse;
-    } catch(error) {
-      return error;
-    }
+  static getWeather(city) {
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+      .then(function(response) {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })      
+      .catch(function(error) {
+        return error;
+      });
   }
 }
+
+
+// static async getWeather(city) {
+//   try {
+//     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`);
+//     const jsonifiedResponse = await response.json();
+//     if (!response.ok) {
+//       const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+//       throw new Error(errorMessage);
+//     }
+//     return jsonifiedResponse;
+//   } catch(error) {
+//     return error;
+//   }
+// }
+
 
 // return new Promise(function(resolve, reject) {
 //   let request = new XMLHttpRequest(); 
