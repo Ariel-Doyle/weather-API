0da9124f23ad7ead197cc4d75c109440590493b3
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-13 20:35:38 -0800

    add syntax for async functions according to lessons.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js'
+import WeatherService from './weather-service.js';
+
+//Business
+async function getWeather(city) {
+  const response = await WeatherService.getWeather(city);
+  if (response.main) {
+    printElements(response, city);
+  } else {
+    printError(response, city);
+  }
+}
 
 // UI Logic
+function printElements(response, city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${response.main.humidity}%.
+  The temperature in Kelvins is ${response.main.temp} degrees or ${((1.8*(response.main.temp - 273)) + 32).toFixed(2)} degrees Fahrenheit.
+  The weather is ${response.weather[0].main}.`;
+}
+
+function printError(error, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  
+    ${error}.`;
+}   
 
 function handleFormSubmission(e) {
   e.preventDefault();
   const city = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
-  let promise = WeatherService.getWeather(city);
-  promise.then(function(response) {
-    printElements(response);
-  }, function(errorMessage) {
-    printError(errorMessage);
-  });
-}
-
-function printElements(results) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${results[1]} is ${results[0].main.humidity}%.
-  The temperature in Kelvins is ${results[0].main.temp} degrees or ${((1.8*(results[0].main.temp - 273)) + 32).toFixed(2)} degrees Fahrenheit.
-  The weather is ${results[0].weather[0].main}.`;
-}
-
-function printError(error) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}:  ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
-}
+  getWeather(city);
+} 
 
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
